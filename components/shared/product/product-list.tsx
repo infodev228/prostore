0e4bb14216de54fdaee552359570353684449bba
Abu Jaid <abujaid@Abus-MacBook-Pro.local>
2025-05-23 16:10:37 +0530

    remove types

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,5 +1,4 @@
 import ProductCard from './product-card';
-import { Product } from '@/types';
 
 const ProductList = ({ data, title, limit }: any) => {
   const limitedData = limit ? data.slice(0, limit) : data;
@@ -9,7 +8,7 @@ const ProductList = ({ data, title, limit }: any) => {
       <h2 className='h2-bold mb-4'>{title}</h2>
       {data.length > 0 ? (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {limitedData.map((product: Product) => (
+          {limitedData.map((product: any) => (
             <ProductCard key={product.slug} product={product} />
           ))}
         </div>
